4dc65d741c41376842303155885ca61dd52c91fc
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-07-10 15:28:25 +0500

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 import cx from 'classnames';
-import deaths from 'src/images/deaths.png';
-import infected from 'src/images/infected.png';
-import recovered from 'src/images/recovered.png';
+import deaths from './deaths.png';
+import infected from './infected.png';
+import recovered from './recovered.png';
 
 import styles from './Cards.module.css';
 
